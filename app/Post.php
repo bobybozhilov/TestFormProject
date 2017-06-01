47d0c56e4f0f3aa31931ee5d7e4b55da9d293d88
bobybozhilov <bobybozhilov@abv.bg>
2017-06-01 11:38:12 +0300

    Added Post model

--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = [
+    protected $fillable =
+        [
+            'name', 'contents'
         ];
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
 }
